6e84f374ad6290790b3bd0ffdba742bbe7981665
Walid Moustafa <waleedaleem@hotmail.com>
2018-09-09 14:43:26 +1000

    introduce command line parameters

--- src/main/java/com/walid/curator/CheckDisconnApp.java
+++ src/main/java/com/walid/curator/CheckDisconnApp.java
@@ -1,5 +1,8 @@
 package com.walid.curator;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.api.CuratorEvent;
@@ -11,47 +14,85 @@ import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 public class CheckDisconnApp implements CuratorListener {
 
-    private static final String CHECK_PATH = "/CMTest/leader";
     private static final Logger logger = LoggerFactory.getLogger(CheckDisconnApp.class);
-    private static final String ZK_CONN_STRING = "127.0.0.1:2181";
-    private static final CuratorFramework CURATOR_FRAMEWORK = CuratorFrameworkFactory.newClient(
-            ZK_CONN_STRING, new ExponentialBackoffRetry(1000, 3));
-    private static String clientId;
+    @Parameter(names = {"--zk-connection", "-z"}, description = "Description: ZK connection string")
+    private String zkConnString = "127.0.0.1:2181";
+    @Parameter(names = {"--zk-leader-path", "-l"}, description = "Description: ZK leader base path")
+    private String checkPath = "/CMTest/leader-election";
+    @Parameter(names = {"--disconn-duration", "-d"}, description = "Description: Configurable connection disruption duration")
+    private int disconnDurationMs = 5000;
+    @Parameter(names = {"--client-id", "-i"}, description = "Description: ZK client ID", required = true)
+    private String clientId = "client1";
+    private CuratorFramework curatorFramework;
 
     public static void main(String[] args) throws Exception {
-        if (args.length != 0) clientId = args[0];
-        CURATOR_FRAMEWORK.getCuratorListenable().addListener(new CheckDisconnApp());
-        CURATOR_FRAMEWORK.start();
+        CheckDisconnApp checkDisconnApp = new CheckDisconnApp();
+        try {
+            JCommander.newBuilder()
+                    .addObject(checkDisconnApp)
+                    .build()
+                    .parse(args);
+        } catch (ParameterException pex) {
+            pex.usage();
+        }
+
+        checkDisconnApp.run();
+
+        System.out.println("Hit Return to quit.");
+        System.in.read();
+    }
+
+    private void run() throws Exception {
+        curatorFramework = CuratorFrameworkFactory.newClient(
+                zkConnString, new ExponentialBackoffRetry(1000, 50));
+        curatorFramework.start();
         for (; ; ) {
             try {
-                final Stat stat = CURATOR_FRAMEWORK.checkExists().watched().forPath(CHECK_PATH);
+                final Stat stat = curatorFramework.checkExists().forPath(checkPath);
                 if (stat != null) {
                     break;
                 } else {
                     Thread.sleep(500);
                 }
             } catch (Exception ex) {
-                logger.debug("Exception caught", ex);
+                logger.debug("Exception caught: ", ex);
             }
         }
-
-        final List<String> children = CURATOR_FRAMEWORK.getChildren().watched().forPath(CHECK_PATH);
+        curatorFramework.getCuratorListenable().addListener(this);
+        final List<String> children = curatorFramework.getChildren().watched().forPath(checkPath);
         if (children.contains(clientId)) {
             disconnect();
         }
-
-        System.in.read();
     }
 
-    private static void disconnect() throws IOException, InterruptedException {
-        new ProcessBuilder("tcpkill port 2181 > /dev/null 2>&1 &").start();
-        Thread.sleep(1000);
-        new ProcessBuilder("pkill -9 tcpkill").start();
+    private void disconnect() throws IOException, InterruptedException {
+        logger.info("Disrupting \"{}\" connection to host \"{}\" for {} milliseconds", clientId, zkConnString, disconnDurationMs);
+        int zkPort;
+        try {
+            zkPort = Integer.parseInt(zkConnString.split(":")[1]);
+        } catch (NumberFormatException nfe) {
+            zkPort = 2181;
+        }
+        logger.debug("Starting disconnect process");
+        new ProcessBuilder("/bin/bash", "-c", String.format("tcpkill port %d", zkPort))
+                .directory(new File("/"))
+                .redirectErrorStream(true)
+                .redirectOutput(ProcessBuilder.Redirect.appendTo(new File("process.log")))
+                .start();
+        Thread.sleep(disconnDurationMs);
+        logger.debug("Stopping disconnect process");
+        new ProcessBuilder("/bin/bash", "-c", "pkill -9 tcpkill")
+                .directory(new File("/"))
+                .redirectErrorStream(true)
+                .redirectOutput(ProcessBuilder.Redirect.appendTo(new File("process.log")))
+                .start();
+        logger.debug("Disrupted \"{}\" connection to host \"{}\" for {} milliseconds", clientId, zkConnString, disconnDurationMs);
     }
 
     @Override
@@ -63,12 +104,15 @@ public class CheckDisconnApp implements CuratorListener {
             return;
         }
 
-        if (CHECK_PATH.equals(eventPath)
-                && watchedEvent.getType().equals(Watcher.Event.EventType.NodeChildrenChanged)
-                && (CURATOR_FRAMEWORK.checkExists().watched().forPath(CHECK_PATH) != null)) {
-            final List<String> children = CURATOR_FRAMEWORK.getChildren().watched().forPath(CHECK_PATH);
-            logger.debug("Current leaders {}", children);
-            if (children.contains(clientId)) disconnect();
+        if (checkPath.equals(eventPath)
+                && watchedEvent.getType().equals(Watcher.Event.EventType.NodeChildrenChanged)) {
+            if (curatorFramework.checkExists().forPath(checkPath) != null) {
+                final List<String> children = curatorFramework.getChildren().watched().forPath(checkPath);
+                logger.debug("Current leaders {}", children);
+                if (children.contains(clientId)) disconnect();
+            } else {
+                logger.debug("Leader base zNode [{}] removed. Current leaders []", checkPath);
+            }
         }
     }
 }
